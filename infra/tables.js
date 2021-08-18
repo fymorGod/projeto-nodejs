459db81ec95b9db1705aea003741cdef96c233e8
fymorgod <mufinagamer@gmail.com>
2021-08-17 22:47:17 -0300

    Regras de Negócio

--- infra/tables.js
+++ infra/tables.js
@@ -6,7 +6,7 @@ class Tables{
     }
 
     criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS atendimentos (id INT NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))';
+        const sql = 'CREATE TABLE IF NOT EXISTS atendimentos (id INT NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes TEXT, PRIMARY KEY(id))';
 
         this.conection.query(sql, (error) =>{
             if(error){
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,49 @@
+const moment = require('moment');
 const conection = require('../infra/conection');
 
 class Atendimento{
-    adiciona(atendimento){
-        const sql = 'INSERT INTO Atendimentos SET ?';
-        conection.query(sql, atendimento, (error, resultados) =>{
-            if(error){
-                console.log(error);
+    adiciona(atendimento, res){ 
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');;
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-            else{
-                console.log(resultados);
-            }
-        });
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros){
+            res.status(400).json(erros)
+        }
+        else{
+            const atendimentoDatado = {...atendimento, dataCriacao, data};
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            conection.query(sql, atendimentoDatado, (error, resultados) =>{
+                if(error){
+                    res.status(400).json(error);
+                }
+                else{
+                    res.status(201).json(atendimento);
+                }
+            });
+        }
+
+        
     }
 }
 module.exports = new Atendimento;
